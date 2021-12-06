b2f1f32faf2cff86b97a95f6e5d0b625bd6341a2
Juan Jimenez <juans.jimenez2890@gmail.com>
2021-12-06 00:40:14 -0500

    Juego ahorcado finalizado

--- JuegoAhorcado.py
+++ JuegoAhorcado.py
@@ -1,23 +1,58 @@
 def run():
     import random
+    import os
     palabras = []
     with open('data.txt', "r", encoding="utf-8") as f:
         for i in f:
             i = i[:-1]
             palabras.append(i)
         
-        print(random.choice(palabras))
+        palabra = random.choice(palabras)
+        
+        resultado = []
+        letras_todas = []
+        intentos = len(palabra)
+        fallos = 0
+        correcta = []
+        
 
-# def run():
-#     # letras_todas = []
-#     # resultado = []
-#     # letras = input('Digita una letra:')
+        while intentos > 0:
+            
+            for i in palabra:
+                if i in letras_todas:
+                    print(i,end="")
+                    
+                    
+                else:
+                    print("_", end="")
+                    fallos += 1
+            if fallos == 0:    
+                print("Ganaste")
+                break
+        
+            letra = input("Introduce una letra:")
+            letras_todas += letra
+            
+            if letra not in palabra:
+                intentos -=1
+                print(f'Restan {intentos} intentos')
+        else:
+            print('Gracias por participar')
+            print(f'{palabra} era la palabra')
+        
+
+        
+      
+
+            
+
+
+
+
+    # for i in range(len(ahorcado())):
+    #      resultado.append("_")
     
-#     print(ahorcado())
-#     # for i in range(len(ahorcado())):
-#     #     resultado.append("_")
-    
-#     #print(resultado)
+    # print(resultado)
 
 
 
